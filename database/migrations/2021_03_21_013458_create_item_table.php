84d50784b059b395ffa7bb6e9a75aa2771833cd8
zerotex <zerotex@mail.com>
2021-03-20 21:13:38 -0500

    Array to json type in ORM

--- database/migrations/2021_03_21_013458_create_item_table.php
+++ database/migrations/2021_03_21_013458_create_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStatsTable extends Migration
+class CreateItemTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class CreateStatsTable extends Migration
     {
         Schema::create("item", function (Blueprint $table) {
             $table->bigIncrements("id");
-            $table->array("stats");
+            $table->json("stats");
             $table->decimal("price");
         });
     }

994113e00cb94d0dc202716f11aab783896862b4
zerotex <zerotex@mail.com>
2021-03-23 20:33:29 -0500

    CRUD methods in ItemController

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
@@ -12,8 +13,60 @@ class ItemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+
+    public function all()
     {
-        return response()->json(Item::all()->jsonSerialize());
+        return Item::all();
+    }
+
+    public function single(Request $item, $id)
+    {
+        $item = Item::find($id);
+        return $item;
+    }
+
+    public function store()
+    {
+        return Item::create([
+            "name" => request("name"),
+            "type" => request("type"),
+            "rarity" => request("rarity"),
+            "max_sockets" => request("max_sockets"),
+            "requirements" => json_encode(request("requirements")),
+            "durability" => request("durability"),
+            "stats" => json_encode(request("stats")),
+            "set_bonuses" => json_encode(request("set_bonuses")),
+            "quantity" => request("quantity"),
+            "price" => request("price"),
+        ]);
+    }
+
+    public function update($item)
+    {
+        $success = $item->update([
+            "name" => request("name"),
+            "type" => request("type"),
+            "rarity" => request("rarity"),
+            "max_sockets" => request("max_sockets"),
+            "requirements" => json_encode(request("requirements")),
+            "durability" => request("durability"),
+            "stats" => json_encode(request("stats")),
+            "set_bonuses" => json_encode(request("set_bonuses")),
+            "quantity" => request("quantity"),
+            "price" => request("price"),
+        ]);
+
+        return [
+            "success" => $success,
+        ];
+    }
+
+    public function delete($item)
+    {
+        $success = $item->destroy();
+
+        return [
+            "success" => $success,
+        ];
     }
 }

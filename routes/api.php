0f4c2ed3ad63a8772dfcb9606c373f70c83329fb
zerotex <zerotex@mail.com>
2021-03-23 20:33:21 -0500

    Finishing up CRUD on API Routes

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,11 @@ use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/item/:id", [ItemController::class, "index"]);
+Route::get("/item/all", [ItemController::class, "all"]);
+Route::get("/item/{id}", [ItemController::class, "single"]);
+
+Route::post("/item/add", [ItemController::class, "store"]);
+
+Route::put("/item/update/{id}", [ItemController::class, "update"]);
+
+Route::delete("/item/delete/{id}", [ItemController::class, "delete"]);

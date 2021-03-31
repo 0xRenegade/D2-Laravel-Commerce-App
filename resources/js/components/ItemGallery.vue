bece02c575608a0145be064e1305565b42033db4
zerotex <zerotex@mail.com>
2021-03-30 21:33:01 -0500

    finish up ItemGallery

--- resources/js/components/ItemGallery.vue
+++ resources/js/components/ItemGallery.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
-    <Item />
+    <h1 class="gallery-title">Item Gallery</h1>
+    <hr />
+    <div class="row">
+      <div
+        v-for="item in items"
+        class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3"
+        :key="item.id"
+      >
+        <Item :item="item" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +20,17 @@ import Item from "./Item";
 export default {
   components: {
     Item,
+  },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost/api/item/all")
+      .then((res) => res.json())
+      .then((data) => (this.items = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>

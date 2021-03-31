c236cf6fb83cd49a0ceb422888e572a668f6c69b
zerotex <zerotex@mail.com>
2021-03-30 21:33:23 -0500

    Finish up Item.vue w/ hover component added

--- resources/js/components/Item.vue
+++ resources/js/components/Item.vue
@@ -1,74 +1,48 @@
 <template>
-  <div id="item-container">
-    <div class="item-name">{{ name }}</div>
-    <div class="item-type">{{ type }}</div>
-    <div class="item-rarity">{{ rarity }}</div>
-    <img
-      src="{{ window.location.href + image_url }}"
-      alt=""
-      class="item-image"
-    />
-    <div class="item-sockets">{{ max_sockets }}</div>
-    <div class="item-requirements">{{ requirements }}</div>
-    <div class="item-durability">{{ durability }}</div>
-    <div class="item-stats">{{ stats }}</div>
-    <div class="item-set-bonuses">{{ set_bonuses }}</div>
-    <div class="quantity">{{ quantity }}</div>
-    <div class="price">{{ price }}</div>
+  <div
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    class="item-container"
+  >
+    <div class="item-name">{{ item.name }}</div>
+    <img :src="url() + item.image_url" alt="" class="item-image" />
+    <div class="quantity">In Stock: {{ item.quantity }}</div>
+    <div class="price">Price: ${{ item.price }}</div>
   </div>
+  <ItemHover
+    v-if="hover"
+    :type="item.type"
+    :rarity="item.rarity"
+    :max_sockets="item.max_sockets"
+    :requirements="item.requirements"
+    :durability="item.durability"
+    :stats="item.stats"
+    :set_bonuses="item.set_bonuses"
+  />
 </template>
 
 <script>
-export default {
-  props: {
-    name: {
-      type: String,
-      required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    rarity: {
-      type: String,
-      required: true,
-    },
-    ethereal: {
-      type: Boolean,
-      required: true,
-    },
+import ItemHover from "./ItemHover";
 
-    max_sockets: {
-      type: Number,
-      required: false,
-    },
-    requirements: {
-      type: Array,
-      required: false,
-    },
-    durability: {
-      type: String,
+export default {
+  name: "Item",
+  components: {
+    ItemHover,
+  },
+  data() {
+    return {
+      hover: false,
+    };
+  },
+  props: {
+    item: {
+      type: Object,
       required: true,
     },
-    stats: {
-      type: Array,
-      required: true,
-    },
-    set_bonuses: {
-      type: Array,
-      required: false,
-    },
-    quantity: {
-      type: Number,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    image_url: {
-      type: String,
-      required: true,
+  },
+  methods: {
+    url: () => {
+      return window.location.href;
     },
   },
 };

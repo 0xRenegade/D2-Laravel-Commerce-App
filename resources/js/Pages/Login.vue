fab9b380a7db79ffd05183789faf4b690ac9ba57
0xRenegade <therenegadecoding@gmail.com>
2021-04-03 23:18:47 -0500

    restyle login page

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,12 +1,26 @@
 <template>
-  <div id="login-modal">
-    <div class="modal-dialog modal-fullscreen">
-      <h3>Login</h3>
-      <p v-if="loading">Loading...</p>
-      <p v-if="error" style="color: red">{{ error }}</p>
-      <input type="text" placeholder="Username" v-model="user.username" /><br />
-      <input type="text" placeholder="Password" v-model="user.password" /><br />
-      <button @click="login">Login</button>
+  <div id="login">
+    <div
+      class="item-container h-100 d-flex justify-content-center align-items-center"
+    >
+      <div class="login-wrapper">
+        <div class="h1">Login:</div>
+        <p v-if="loading">Loading...</p>
+        <p v-if="error" style="color: red">{{ error }}</p>
+        <input
+          class="border-dark rounded-2 login-input"
+          type="text"
+          placeholder="Username"
+          v-model="user.username"
+        /><br />
+        <input
+          class="border-dark rounded-2 login-input"
+          type="text"
+          placeholder="Password"
+          v-model="user.password"
+        /><br />
+        <button @click="login" class="btn btn-dark btn-login">Login</button>
+      </div>
     </div>
   </div>
 </template>

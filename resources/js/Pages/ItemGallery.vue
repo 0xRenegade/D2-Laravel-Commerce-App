7ba16b797c08f5cd29dcf87dc0e21dad68c84ef8
0xRenegade <therenegadecoding@gmail.com>
2021-04-03 22:38:57 -0500

    add margin && adjust url in fetch

--- resources/js/Pages/ItemGallery.vue
+++ resources/js/Pages/ItemGallery.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="site-title">Item Gallery</h1>
     <hr />
-    <div class="row">
+    <div class="row mt-5">
       <div
         v-for="item in items"
         class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3"
@@ -27,7 +27,7 @@ export default {
     };
   },
   mounted() {
-    fetch("http://localhost/api/item/all")
+    fetch(window.location.origin + "/api/item/all")
       .then((res) => res.json())
       .then((data) => (this.items = data))
       .catch((err) => console.log(err.message));

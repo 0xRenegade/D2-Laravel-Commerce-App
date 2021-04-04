25c91a976299d420d161b9d4aa35b3efa00d906f
0xRenegade <therenegadecoding@gmail.com>
2021-04-03 22:39:25 -0500

    Link to ItemPage components

--- resources/js/Components/Item.vue
+++ resources/js/Components/Item.vue
@@ -1,40 +1,21 @@
 <template>
-  <div
-    @mouseenter="showTooltip = true"
-    @mousemove="hoverFunc($event)"
-    @mouseleave="showTooltip = false"
-    class="item-container"
-  >
-    <div class="item-name">{{ item.name }}</div>
-    <img :src="url() + item.image_url" alt="" class="item-image" />
-    <div class="quantity">In Stock: {{ item.quantity }}</div>
-    <div class="price">Price: ${{ item.price }}</div>
-  </div>
-  <ItemHover
-    v-show="showTooltip"
-    :type="item.type"
-    :rarity="item.rarity"
-    :max_sockets="item.max_sockets"
-    :requirements="item.requirements"
-    :durability="item.durability"
-    :stats="item.stats"
-    :set_bonuses="item.set_bonuses"
-  />
+  <router-link :to="{ name: 'item', params: { id: item.id } }">
+    <div
+      @mouseenter="hoverFunc($event, true)"
+      @mouseleave="hoverFunc($event, false)"
+      class="item-container"
+    >
+      <div class="item-name">{{ item.name }}</div>
+      <img :src="url() + item.image_url" alt="" class="item-image" />
+      <div class="quantity">In Stock: {{ item.quantity }}</div>
+      <div class="price">Price: ${{ item.price }}</div>
+    </div>
+  </router-link>
 </template>
 
 <script>
-import ItemHover from "./ItemHover";
-
 export default {
   name: "Item",
-  components: {
-    ItemHover,
-  },
-  data() {
-    return {
-      showTooltip: false,
-    };
-  },
   props: {
     item: {
       type: Object,
@@ -46,9 +27,12 @@ export default {
       const url = window.location.origin + "/";
       return url;
     },
-    hoverFunc(event) {
-      event.currentTarget.nextSibling.style.left = event.layerX - 200 + "px";
-      event.currentTarget.nextSibling.style.top = event.layerY + 20 + "px";
+    hoverFunc(event, hover) {
+      if (hover) {
+        event.currentTarget.style.transform = "translateY(-10px)";
+      } else {
+        event.currentTarget.style.transform = "translateY(0px)";
+      }
     },
   },
 };
